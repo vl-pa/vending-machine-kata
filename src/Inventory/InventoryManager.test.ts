1495619bf4aa8822b974888e17044fe329bef913
Vladimir Pavlicenko <vladimir@pavlicenko.com>
2020-07-18 13:17:59 +0100

    implement subtractFromInventory + unit tests and refactor

--- src/Inventory/InventoryManager.test.ts
+++ src/Inventory/InventoryManager.test.ts
@@ -18,16 +18,28 @@ describe("InventoryManager", () => {
         name: ProductName.chips,
         price: 2
       },
-      quantity: 0
+      quantity: 2
     }
   ]
   const inventoryManager = new InventoryManager(testInventory);
 
   it("should return true if product is in stock", () => {
-    expect(inventoryManager.productIsInStock(ProductName.cola)).toEqual(true);
+    expect(inventoryManager.getProductStock(ProductName.cola)).toEqual(10);
   });
 
   it("should return false if product is out of stock", () => {
-    expect(inventoryManager.productIsInStock(ProductName.chips)).toEqual(false);
+    expect(inventoryManager.getProductStock(ProductName.chips)).toEqual(2);
+  });
+
+  it("should subtract item from inventory", () => {
+    expect(inventoryManager.getProductStock(ProductName.cola)).toEqual(10);
+    inventoryManager.subtractFromInventory(ProductName.cola);
+    expect(inventoryManager.getProductStock(ProductName.cola)).toEqual(9);
+  });
+
+  it("should not subtract item from inventory that hs not been selected", () => {
+    expect(inventoryManager.getProductStock(ProductName.chips)).toEqual(2);
+    inventoryManager.subtractFromInventory(ProductName.cola);
+    expect(inventoryManager.getProductStock(ProductName.chips)).toEqual(2);
   });
 });
--- src/Inventory/InventoryManager.ts
+++ src/Inventory/InventoryManager.ts
@@ -2,26 +2,41 @@ import { Product, ProductName } from "./Product";
 import { Inventory } from "./Inventory";
 
 interface IInventoryManager {
-  productIsInStock: (product: ProductName) => boolean;
+  getProductStock: (product: ProductName) => number;
   subtractFromInventory: (Product: ProductName) => void;
 }
 
 export class InventoryManager implements IInventoryManager {
- private availableProducts: Inventory[];
+  private inventory: Inventory[];
 
- constructor(productsForSale: Inventory[]) {
-   this.availableProducts = productsForSale;
- }
+  constructor(productsForSale: Inventory[]) {
+    this.inventory = productsForSale;
+  }
 
- public productIsInStock(productName: ProductName) {
-   const product = this.availableProducts.find(product => productName === product.product.name);
-   if (product === undefined) {
-     return false;
-   } 
-   return product.quantity > 0 ? true : false;
- }
+  private getInventoryItem(productName: ProductName): Inventory | undefined {
+    return this.inventory.find(product => productName === product.product.name);
+  }
 
- public subtractFromInventory() {
-   
- }
+  public getProductStock(productName: ProductName) {
+    const product = this.getInventoryItem(productName);
+    if (product === undefined) {
+      return 0;
+    }
+    return product.quantity;
+  }
+
+  public subtractFromInventory(productName: ProductName) {
+    const inventoryItem = this.getInventoryItem(productName);
+    if (inventoryItem !== undefined) {
+      this.inventory = this.inventory.map(x => {
+        if (x.product.name === inventoryItem.product.name) {
+          return {
+            ...x,
+            quantity: x.quantity - 1
+          }
+        }
+        return x;
+      });
+    }
+  }
 }
